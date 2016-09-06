afdb38c2acab638ccd35ff4a7e3a60db3d9adda6
James Welch <james.welch@cs.ox.ac.uk>
2016-09-06 16:53:56 +0100

    Data Class -> New Child Data Class service

--- MetadataCatalogue/src/test/java/ox/softeng/metadatacatalogue/test/restapi/DataClassServiceIT.java
+++ MetadataCatalogue/src/test/java/ox/softeng/metadatacatalogue/test/restapi/DataClassServiceIT.java
@@ -69,6 +69,53 @@ public class DataClassServiceIT extends APITest {
 		assertTrue(value.equalsIgnoreCase("value1"));
 
 	}
+	
+	@FlywayTest(invokeCleanDB=true, invokeBaselineDB=true)
+	@Test
+	public void addChildClass() throws Exception {
+		
+		Response response = doLogin();
+		String sessionId = getSessionCookie(response);
+		
+		
+		DataSet ds = DataSetApi.createDataSet(apiCtx, "Test Dataset", "This is just a test dataset", "Author", "Organization");
+		DataClass dc = DataModelApi.newDataClass(apiCtx, ds, "class1", "class1desc");
+		System.err.println(dc.getId());
+		
+		String path = "/dataclass/newChildDataClass/" + dc.getId();
+		WebTarget resource = target.path(path);
+		Invocation.Builder invocationBuilder = resource.request(MediaType.APPLICATION_JSON).cookie("JSESSIONID", sessionId);
+		
+		DataClass newDC = new DataClass();
+		newDC.setLabel("my test class");
+		newDC.setDescription("my test class description");
+		String json = objectMapper.writeValueAsString(newDC);
+		System.err.println(json);
+		response = invocationBuilder.post(Entity.entity(json, MediaType.APPLICATION_JSON));
+		assertTrue(response.getStatus()==200);
+		
+		ResponseDTO respObj = (ResponseDTO) response.readEntity(ResponseDTO.class);
+		assertTrue(respObj.isSuccess());
+		assertTrue(respObj.getErrorMessages() == null || respObj.getErrorMessages().size() == 0);
+		assertTrue(respObj.getReturnObjectType().contains("DataClass"));
+		JsonNode jn1 = respObj.getReturnObject();
+		String label1 = jn1.get("label").asText();
+		String description1 = jn1.get("description").asText();
+		assertTrue(label1.equalsIgnoreCase("my test class"));
+		assertTrue(description1.equalsIgnoreCase("my test class description"));
+		String newId = jn1.get("id").asText();
+		assertTrue(newId != null && !newId.equals(""));
+		
+		JsonNode jn = apiCtx.getByIdMap(DataClass.class, "dataclass.pageview.id", dc.getId());
+		ArrayNode an = (ArrayNode) jn.get("childDataClasses");
+		
+		assertTrue(an.size() == 1);
+		String key = an.get(0).get("label").asText();
+		String value = an.get(0).get("description").asText();
+		assertTrue(key.equalsIgnoreCase("my test class"));
+		assertTrue(value.equalsIgnoreCase("my test class description"));
+
+	}
 
 	
 }
